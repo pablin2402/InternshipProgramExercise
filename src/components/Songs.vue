5bc0c9b4a624e3789231b57b70afe1d46b6afb01
pablin2402 <pabloperedo04@gmail.com>
2021-01-22 22:51:12 -0400

    Presentation of some songs with data.json into cards using Vuetify

--- src/components/Songs.vue
+++ src/components/Songs.vue
@@ -1,6 +1,63 @@
 <template>
   <v-container>
-    <h1>CANCIONES ITUNES</h1>
+    <h1>Canciones Itunes</h1>
+    <br />
+    <v-text-field
+      placeholder="Busca tu canción favorita en Itunes"
+      filled
+      rounded
+      dense
+    ></v-text-field>
+    <v-row dense>
+      <v-col cols="12">
+        <v-card color="#385F73" dark>
+          <v-card-title class="headline"> Unlimited music now </v-card-title>
+
+          <v-card-subtitle
+            >Listen to your favorite artists and albums whenever and wherever,
+            online and offline.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn text> Listen Now </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col v-for="(item, i) in items" :key="i" cols="12">
+        <v-card :color="item.color" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn
+                  v-if="item.artist === 'Ellie Goulding'"
+                  class="ml-2 mt-3"
+                  fab
+                  icon
+                  height="40px"
+                  right
+                  width="40px"
+                >
+                  <v-icon>mdi-play</v-icon>
+                </v-btn>
+
+                <v-btn v-else class="ml-2 mt-5" outlined rounded small>
+                  START RADIO
+                </v-btn>
+              </v-card-actions>
+            </div>
+
+            <v-avatar class="ma-3" size="125" tile>
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -8,6 +65,21 @@
 export default {
   name: "Songs",
 
-  data: () => ({}),
+  data: () => ({
+    items: [
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#952175",
+        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
+        title: "Halcyon Days",
+        artist: "Ellie Goulding",
+      },
+    ],
+  }),
 };
 </script>

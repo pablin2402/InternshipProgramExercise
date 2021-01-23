468517d414add80fafeb1f5db357dcfda1dbe55b
pablin2402 <pabloperedo04@gmail.com>
2021-01-23 02:19:27 -0400

    Search field is done, need to improve UI and optional points

--- src/components/Songs.vue
+++ src/components/Songs.vue
@@ -1,26 +1,15 @@
 <template>
   <v-container>
+    <h1>Canciones Itunes</h1>
+    <v-text-field
+      placeholder="Busca tu canción favorita en Itunes"
+      v-model="selectedSong"
+      append-icon="mdi-magnify"
+      outlined
+      rounded
+    ></v-text-field>
+    <v-btn color="primary" v-on:click="getByName"> Primary </v-btn>
     <v-row dense>
-      <h1>Canciones Itunes</h1>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <v-text-field
-        placeholder="Busca tu canción favorita en Itunes"
-        v-model="selectedSong"
-        filled
-        rounded
-        dense
-      ></v-text-field>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
       <v-col
         v-for="(item, i) in listOfAlbums"
         :key="i"
@@ -32,20 +21,21 @@
             :src="item.artworkUrl100"
             class="white--text align-end"
             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
+            height="300px"
           >
-            <v-card-title v-text="item.collectionName"></v-card-title>
           </v-img>
           <div class="d-flex flex-no-wrap justify-space-between">
             <div>
               <v-card-title
-                class="headline"
                 v-text="item.collectionName"
+                class="font-weight-light"
               ></v-card-title>
 
               <v-card-subtitle v-text="item.artistName"></v-card-subtitle>
 
-              <v-card-actions> </v-card-actions>
+              <v-card-actions>
+                <v-btn text> Price {{ item.collectionPrice }} $ </v-btn>
+              </v-card-actions>
             </div>
           </div>
         </v-card>
@@ -56,6 +46,8 @@
 
 <script>
 import axios from "axios";
+import { API_HOST } from "../utils/constant";
+import { config } from "../utils/config";
 
 export default {
   name: "Songs",
@@ -81,56 +73,23 @@ export default {
   }),
 
   mounted() {
-    this.getArtistId();
-    //  this.getAlbumsByArtist();
+    this.getByName();
   },
   methods: {
-    async getArtistId() {
-      var config = {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Credentials": "true",
-        },
-      };
-
-      await axios
-        .get(
-          "https://itunes.apple.com/search?term=jack+johnson&limit=10/",
-          config
-        )
+    async getByName() {
+      var self = this;
+      axios
+        .get(API_HOST + "term=" + self.selectedSong + "&limit=25/", config)
         .then((response) => {
           this.listOfAlbums = response.data.results;
-
-          console.log(this.listOfAlbums);
         })
         .catch((error) => {
           console.log(error);
         });
     },
-    async getAlbumsByArtist() {
-      var config = {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Credentials": "true",
-        },
-      };
-
-      await axios
-        .get("https://itunes.apple.com/lookup?id=909253&entity=album ", config)
-        .then((response) => {
-          this.listOfAlbums = response.data;
-          console.log(this.listOfAlbums);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
+  },
+  computed() {
+    this.getByName();
   },
 };
 </script>
